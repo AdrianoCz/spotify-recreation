066fb9c15748b7e539acd77a5dc485bd76756415
AdrianoCz <adrianozx2007@gmail.com>
2026-05-16 10:27:51 -0300

    add site

---
+++ src/App.jsx
@@ -0,0 +1,115 @@
+import { useState } from 'react'
+import { useRef } from 'react'
+import { useEffect } from 'react'
+import Play from "./assets/play.svg"
+import Skip from "./assets/skip.svg"
+import Back from "./assets/back.svg"
+import Pause from "./assets/pause.svg"
+import Audio from "./assets/audio.svg"
+import barelylegal from "./assets/barelylegal.mp3"
+import cover from "./assets/isthisit.jpg"
+import './App.css'
+
+
+function Main({ song }) {
+  return (
+    <div id='main'>
+      <div style={{backgroundImage: "url(" + song.cover +")"}} id="big-image"></div>
+      <div  id='hover'>
+        <div id='song-info' >
+        <div id="song">{song.name}</div>
+        <div id="artist">{song.artist}</div></div>
+        <p id="album">{song.album}</p>
+        
+      </div>
+      
+      
+    </div>
+  )
+}
+function Control({ song }) {
+  const audioRef = useRef();
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = 0.5; 
+    }
+  }, []);
+  const getTempo = () => {
+    if (audioRef.current) {
+      let minutes = (Math.floor(audioRef.current.currentTime / 60)).toString()
+      let seconds = (Math.floor(audioRef.current.currentTime % 60)).toString()
+      if (seconds.length < 2) {
+        seconds = "0" + seconds
+      }
+      if (minutes.length < 2) {
+        minutes = "0" + minutes
+      }
+      setCurrent(`${minutes}:${seconds}`)
+    }
+  }
+  const mudarTempo = (e) => {
+    if (audioRef.current) {
+      audioRef.current.currentTime
+    }
+
+  }
+  const mudarVolume = (x) => {
+    if (audioRef.current) {
+      audioRef.current.volume = x / 100;
+    }
+  }
+  const [playing, setPlay] = useState(false);
+  const [volume, setVolume] = useState(50);
+  const [currentTime, setCurrent] = useState("00:00");
+
+  return (
+    <div id='control'>
+      <div id='playing-info'>
+        <div style={{backgroundImage: "url(" + song.cover +")"}} id='album-image'></div>
+        <div id='nartist'>
+          <p id='playing-name'>{song.name}</p>
+          <p id="playing-artist">{song.artist}</p>
+
+        </div>
+        <div id="playing">
+          <div id='music-control'>
+            <img className='fb' onClick={() => { audioRef.current.currentTime = 0; audioRef.current.pause(); setPlay(false) }} src={Back} alt="" />
+            <img id='play-button' onClick={() => { setPlay(!playing); { playing ? audioRef.current.pause() : audioRef.current.play() } }} src={playing ? Pause : Play} alt="" />
+            <img className='fb' onClick={() => { audioRef.current.currentTime = 234; audioRef.current.pause(); setPlay(false); }} src={Skip} alt="" />
+          </div>
+          <div id='duration'>
+            <span>{currentTime}</span>
+            <div id='bar'>
+              <div style={{ width: audioRef.current ? ((audioRef.current.currentTime / 234) * 100 * 30 / 100) + "rem" : "0rem" }} id="time-bar"></div>
+              <input type="range" name="" min="0" max="234" step="1" onChange={(e) => { audioRef.current.currentTime = e.target.value }} id="time-input" /></div>
+            <span>{song.duration}</span>
+          </div>
+
+        </div>
+
+      </div>
+      <div id='audio-control'>
+        <img id='audio-icon' src={Audio} alt="" />
+        <div id='audio-bar'><div style={{ width: (5 * volume) / 100 + "rem" }} id='volume'></div>
+          <input style={{ width: "5rem", height: "4px" }} type="range" name="" id="volume-changer" onChange={(e) => { setVolume(e.target.value); console.log(volume); mudarVolume(e.target.value) }} min="0" max="100" step="0.5" value={volume} /></div>
+      </div>
+      <audio volume="50" onTimeUpdate={getTempo} src={song.mp3} ref={audioRef} id="audio">
+      </audio>
+    </div>
+  )
+
+}
+function App() {
+
+
+  const song = { name: "Barely Legal", artist: "The Strokes", mp3: barelylegal, album: "Is This It?", cover: cover, duration: "3:54" }
+  return (
+
+    <>
+      <Main song={song} />
+      <Control song={song} />
+    </>
+  )
+}
+
+export default App
---
+++ src/icons.jsx
@@ -0,0 +1,24 @@
+function Audio(){
+    return(
+        <span aria-hidden="true" class="e-10451-button__icon-wrapper"><svg data-encore-id="icon" role="presentation" aria-label="Volume alto" aria-hidden="false" class="e-10451-icon" style="--encore-icon-height: var(--encore-graphic-size-informative-smaller); --encore-icon-width: var(--encore-graphic-size-informative-smaller);" id="volume-icon" viewBox="0 0 16 16"><path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.64 3.64 0 0 1-1.33-4.967 3.64 3.64 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.14 2.14 0 0 0 0 3.7l5.8 3.35V2.8zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88"></path><path d="M11.5 13.614a5.752 5.752 0 0 0 0-11.228v1.55a4.252 4.252 0 0 1 0 8.127z"></path></svg></span>
+    )
+}
+function Play() {
+    return(<svg height="200px" width="200px" version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <style type="text/css"> fill:#ffffff; </style> <g> <path class="st0" d="M256,0C114.625,0,0,114.625,0,256c0,141.374,114.625,256,256,256c141.374,0,256-114.626,256-256 C512,114.625,397.374,0,256,0z M351.062,258.898l-144,85.945c-1.031,0.626-2.344,0.657-3.406,0.031 c-1.031-0.594-1.687-1.702-1.687-2.937v-85.946v-85.946c0-1.218,0.656-2.343,1.687-2.938c1.062-0.609,2.375-0.578,3.406,0.031 l144,85.962c1.031,0.586,1.641,1.718,1.641,2.89C352.703,257.187,352.094,258.297,351.062,258.898z"></path> </g> </g></svg>)
+}
+function Pause() {
+    return(
+<span class="e-10451-overflow-wrap-anywhere e-10451-button-primary__inner encore-inverted-light-set e-10451-button-icon-only--small"><span aria-hidden="true" class="e-10451-button__icon-wrapper"><svg data-encore-id="icon" role="img" aria-hidden="true" class="e-10451-icon" style="--encore-icon-height: var(--encore-graphic-size-decorative-smaller); --encore-icon-width: var(--encore-graphic-size-decorative-smaller);" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7z"></path></svg></span></span>
+    )
+}
+function Skip() {
+    return(
+        <span aria-hidden="true" class="e-10451-button__icon-wrapper"><svg data-encore-id="icon" role="img" aria-hidden="true" class="e-10451-icon" style="--encore-icon-height: var(--encore-graphic-size-decorative-smaller); --encore-icon-width: var(--encore-graphic-size-decorative-smaller);" viewBox="0 0 16 16"><path d="M12.7 1a.7.7 0 0 0-.7.7v5.15L2.05 1.107A.7.7 0 0 0 1 1.712v12.575a.7.7 0 0 0 1.05.607L12 9.149V14.3a.7.7 0 0 0 .7.7h1.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7z"></path></svg></span>
+    )
+}
+function Back() {
+    return(
+        <span aria-hidden="true" class="e-10451-button__icon-wrapper"><svg data-encore-id="icon" role="img" aria-hidden="true" class="e-10451-icon" style="--encore-icon-height: var(--encore-graphic-size-decorative-smaller); --encore-icon-width: var(--encore-graphic-size-decorative-smaller);" viewBox="0 0 16 16"><path d="M3.3 1a.7.7 0 0 1 .7.7v5.15l9.95-5.744a.7.7 0 0 1 1.05.606v12.575a.7.7 0 0 1-1.05.607L4 9.149V14.3a.7.7 0 0 1-.7.7H1.7a.7.7 0 0 1-.7-.7V1.7a.7.7 0 0 1 .7-.7z"></path></svg></span>
+    )
+}
+export default Back
